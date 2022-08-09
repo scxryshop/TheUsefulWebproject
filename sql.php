919656a9a0a87a5170509ea03509bca1b0cbd958
Zäch <60199908+scxryshop@users.noreply.github.com>
2022-08-09 08:45:53 +0200

    Added Clicks and Odered by it

--- sql.php
+++ sql.php
@@ -1,67 +1,100 @@
-<?php 
+<?php
 
-define("DB_NAME","theusefulwebproject");
+define("DB_NAME", "theusefulwebproject");
 define("DB_HOST", "localhost");
-define("DB_PWD","");
-define("DB_USER","root");
+define("DB_PWD", "");
+define("DB_USER", "root");
 
 
-function fetchAllData(){
+function fetchAllData()
+{
     $conn = mysqli_connect(DB_HOST, DB_USER, DB_PWD, DB_NAME);
-    $sql = "SELECT name, address FROM links limit 0,6";
+    $sql = "SELECT PKLinks, name, address FROM links ORDER BY clicks DESC limit 0,6";
     $stmt = mysqli_stmt_init($conn);
-    if(!mysqli_stmt_prepare($stmt, $sql)){
+    if (!mysqli_stmt_prepare($stmt, $sql)) {
         exit();
     }
     mysqli_stmt_execute($stmt);
     $result = mysqli_stmt_get_result($stmt);
-    while($row = mysqli_fetch_assoc($result)){
-        echo "<a href='".$row['address']."'><button class='dataset'>".$row['name']."</button></a><br>";
+    while ($row = mysqli_fetch_assoc($result)) {
+        echo "
+            <form action=' ' method='POST'>
+            <input type='hidden' name='PK' value=" . $row['PKLinks'] . ">
+            <input type='hidden' name='link-address' value=" . $row['address'] . ">
+            <button type='submit' name='submit-click' class='dataset'>" . $row['name'] . "
+                <img id='q" . $row['PKLinks'] . "' class='question' src='question-circle.svg'>
+            </button>
+            </form><br>";
     }
     mysqli_close($conn);
 }
 
-function fetchCategories(){
+if (isset($_POST['submit-click'])) {
+    $conn = mysqli_connect(DB_HOST, DB_USER, DB_PWD, DB_NAME);
+    $sql = "UPDATE links SET clicks = clicks+1 WHERE ".$_POST['PK']." = PKLinks";
+    $stmt = mysqli_stmt_init($conn);
+    if (!mysqli_stmt_prepare($stmt, $sql)) {
+        exit();
+    }
+    mysqli_stmt_execute($stmt);
+    mysqli_close($conn);
+    header("Location: ".$_POST['link-address']);
+    exit();
+}
+
+function fetchCategories()
+{
     $conn = mysqli_connect(DB_HOST, DB_USER, DB_PWD, DB_NAME);
     $sql = "SELECT name, PKCategories FROM categories";
     $stmt = mysqli_stmt_init($conn);
-    if(!mysqli_stmt_prepare($stmt, $sql)){
+    if (!mysqli_stmt_prepare($stmt, $sql)) {
         exit();
     }
     mysqli_stmt_execute($stmt);
     $result = mysqli_stmt_get_result($stmt);
-    while($row = mysqli_fetch_assoc($result)){
-        echo "<option value='".$row['PKCategories']."'>".$row['name']."</option>";
+    while ($row = mysqli_fetch_assoc($result)) {
+        echo "<option value='" . $row['PKCategories'] . "'>" . $row['name'] . "</option>";
     }
     mysqli_close($conn);
 }
 
-function searchAllData($search){
+function searchAllData($search)
+{
     $conn = mysqli_connect(DB_HOST, DB_USER, DB_PWD, DB_NAME);
-    $s = "%".$search."%";
+    $s = "%" . $search . "%";
     $sql = "SELECT name, address FROM links WHERE name LIKE $s";
     $stmt = mysqli_stmt_init($conn);
-    if(!mysqli_stmt_prepare($stmt, $sql)){
+    if (!mysqli_stmt_prepare($stmt, $sql)) {
         exit();
     }
     mysqli_stmt_bind_param($stmt, "s", $search);
     mysqli_stmt_execute($stmt);
     $result = mysqli_stmt_get_result($stmt);
-    while($row = mysqli_fetch_assoc($result)){
-        echo "<a href='".$row['address']."'><button class='dataset'>".$row['name']."</button></a><br>";
+    while ($row = mysqli_fetch_assoc($result)) {
+        echo "<a href='" . $row['address'] . "'><div><button class='dataset'>" . $row['name'] . "<img id='q" . $row['PKLinks'] . "' class='question' src='question-circle.svg'></button></div></a><br>";
     }
     mysqli_close($conn);
 }
 
 ?>
 <style>
-    a a:hover{
+    a a:hover {
         text-decoration: none;
     }
-    button:hover{
-        cursor: pointer;    
-    }   
-    .dataset{
+
+    button:hover {
+        cursor: pointer;
+    }
+
+    .question {
+        position: absolute;
+        right: 4%;
+        width: 3vh;
+        height: 3vh;
+        z-index: 3;
+    }
+
+    .dataset {
         width: 100%;
         height: 6vh;
         padding: 10px 20px;
